b01cda0064bb6ad538344edb3c39c548b56860e0
Hugh Takerei <hugh.takerei@aucklandcouncil.govt.nz>
2024-06-07 14:48:19 +1200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Tab from './components/Tab';
-import KoreaPage from './components/KoreaPage';
-import JapanPage from './components/JapanPage';
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
-  const [activeTab, setActiveTab] = useState('korea');
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <div className="tabs">
-        <Tab
-          label="Korea"
-          isActive={activeTab === 'korea'}
-          onClick={() => handleTabClick('korea')}
-        />
-        <Tab
-          label="Japan"
-          isActive={activeTab === 'japan'}
-          onClick={() => handleTabClick('japan')}
-        />
-      </div>
-      <div className="content">
-        {activeTab === 'korea' && <KoreaPage />}
-        {activeTab === 'japan' && <JapanPage />}
-      </div>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
