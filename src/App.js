744f8bd43e52754199d60f661d4efcec113fa71e
Hugh Takerei <hughtakk@gmail.com>
2024-06-11 16:21:15 +1200

    switch from browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Itinerary from './components/Itinerary';
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
         <Route path="/itinerary" element={<Itinerary />} />
+        <Route path="/japan" element={<div>Japan Page</div>} />
+        <Route path="/korea" element={<div>Korea Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
